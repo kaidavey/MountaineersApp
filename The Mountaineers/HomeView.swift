678893a968b86e13d574416748df0a0a24ed7b70
kaidavey <k.davey4@icloud.com>
2024-06-29 16:15:35 -0700

    Background effects.

---
+++ The Mountaineers/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  The Mountaineers
+//
+//  Created by Hong Huang on 6/29/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
