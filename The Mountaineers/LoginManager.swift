00d75438d457eadf5584416545cca7677b52c3c5
kaidavey <k.davey4@icloud.com>
2024-06-29 13:33:12 -0700

    Login view complete.

---
+++ The Mountaineers/LoginManager.swift
@@ -0,0 +1,8 @@
+//
+//  LoginManager.swift
+//  The Mountaineers
+//
+//  Created by Hong Huang on 6/29/24.
+//
+
+import Foundation
---
+++ The Mountaineers/LoginView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  The Mountaineers
+//
+
+import SwiftUI
+import SwiftData
+
+struct LoginView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    LoginView()
+}
